41727243a1c49db3af24fb879d1fc006534090eb
cuongyura <c95.ficf@gmail.com>
2020-08-19 14:42:25 +0700

    update

---
+++ src/_15_XuLyNgoaiLe/bai_Tap/_1_Using_Class_IllegalTriangleException/Triangle.java
@@ -0,0 +1,17 @@
+package _15_XuLyNgoaiLe.bai_Tap._1_Using_Class_IllegalTriangleException;
+
+import java.util.Scanner;
+
+public class Triangle {
+    public static void main(String[] args) {
+        Triangle triangle = new Triangle();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a: ");
+        int a = sc.nextInt();
+        System.out.println("Enter a: ");
+        int b = sc.nextInt();
+        System.out.println("Enter a: ");
+        int c = sc.nextInt();
+    }
+
+}
