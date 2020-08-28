fc9d5d0bd510df91fbc2a1969414eb9aab27965a
cuongyura <c95.ficf@gmail.com>
2020-08-28 16:55:15 +0700

    upadte

--- src/Castudymd2/_FuramaResort/Controllers/MainController.java
+++ src/Castudymd2/_FuramaResort/Controllers/MainController.java
@@ -1,10 +1,13 @@
 package Castudymd2._FuramaResort.Controllers;
 
 import Castudymd2._FuramaResort.Models.*;
+import _11_DSA_Stack_Queue._GenericTest.GenericTest;
 
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MainController {
     // Tạo đường dẫn file:
@@ -14,9 +17,13 @@ public class MainController {
     public static final String VILLA_CSV = "E:\\C0620G1_Ho_Manh_Cuong\\module2\\src\\Castudymd2\\_FuramaResort\\Data\\Villa.csv";
     public static final String CUSTOMER_CSV = "E:\\C0620G1_Ho_Manh_Cuong\\module2\\src\\Castudymd2\\_FuramaResort\\Data\\Customer.csv";
 
-    public static void main(String[] args) {
-        displayMainMenu();
+    public static void main(String[] args) throws IOException {
+        Customer customer = new Customer();
+        addCustomer();
+        System.out.println(customer.showInfor());
     }
+
+
     public static void displayMainMenu(){
          Scanner sc = new Scanner(System.in);
             int menuChoice;
@@ -128,7 +135,7 @@ public class MainController {
     }
 
     // add house:
-    public static void House() throws IOException {
+    public static void addHouse() throws IOException {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter ID: ");
         String id = sc.nextLine();
@@ -163,20 +170,74 @@ public class MainController {
     }
         // add customrer
 
-        public static void Customer() throws IOException {
+        public static void addCustomer() throws IOException {
+
             Scanner sc = new Scanner(System.in);
-            System.out.println("Enter nameCustomer is: ");
-            String nameCustomer = sc.nextLine();
-            System.out.println("Enter birthday is: ");
-            String birthDay = sc.nextLine();
-            System.out.println("Enter Sex is: ");
-            String sex = sc.nextLine();
+            String id;
+            boolean flag;
+            do {
+                final String REGEX_ID = "\\d{9}";
+                System.out.println("Enter ID is: ");
+                id = sc.nextLine();
+                Pattern pattern = Pattern.compile(REGEX_ID);
+                Matcher matcher = pattern.matcher(id);
+                flag = matcher.matches();
+                if (!flag) System.out.println("Error! Is card have 9 number! ");
+            }while (!flag);
+            String nameCustomer;
+
+            do {
+                final String REGEX_CUSTOMER = "[A-Z][a-z]+(\\s[A-Z][a-z]+)+$";
+                System.out.println("Enter Name Customer is: ");
+                nameCustomer = sc.nextLine();
+                Pattern pattern = Pattern.compile(REGEX_CUSTOMER);
+                Matcher matcher = pattern.matcher(nameCustomer);
+                flag = matcher.matches();
+                if (!flag) System.out.println("Error! ");
+            }while (!flag);
+            flag = false;
+            String birthDay;
+            do{
+                final String REGEX_EMAIL = "^(0?[1-9]|[12][0-9]|3[01])[\\/\\-](0?[1-9]|1[012])[\\/\\-]\\d{4}$";
+                System.out.println("Enter birthDay is: ");
+                birthDay = sc.nextLine();
+                Pattern pattern = Pattern.compile(REGEX_EMAIL);
+                Matcher matcher = pattern.matcher(birthDay);
+                flag = matcher.matches();
+
+                if(!flag) System.out.println("Error! birthday have format dd/MM/yyyy ");;
+            }while(!flag);
+            flag = false;
+            String gender;
+            do{
+                    final String REGEX_GENDER = "^Male$|^Female$|^Unknow$|^uNKNOW$|^mALE$|^fEMALE$";
+                System.out.println("Enter gender is: ");
+                gender = sc.nextLine();
+                gender = gender.toLowerCase();
+                gender = Character.toUpperCase(gender.charAt(0)) + gender.substring(1);
+                Pattern pattern = Pattern.compile(REGEX_GENDER);
+                Matcher matcher = pattern.matcher(gender);
+                flag = matcher.matches();
+
+                if(!flag) System.out.println("Error!");;
+            }while(!flag);
             System.out.println("Enter cmnd is: ");
             String cmnd = sc.nextLine();
             System.out.println("Enter phone is: ");
             String phone = sc.nextLine();
-            System.out.println("Enter Email is: ");
-            String email = sc.nextLine();
+            flag = false;
+            String email;
+            do{
+                final String REGEX_EMAIL = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+                                              + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+                System.out.println("Enter email is: ");
+                email = sc.nextLine();
+                Pattern pattern = Pattern.compile(REGEX_EMAIL);
+                Matcher matcher = pattern.matcher(email);
+                flag = matcher.matches();
+
+                if(!flag) System.out.println("Error! ");;
+            }while(!flag);
             System.out.println("Enter typeCustomer: ");
             String typeCustomer = sc.nextLine();
             System.out.println("Enter address is: ");
@@ -184,7 +245,7 @@ public class MainController {
             System.out.println("Enter useService is: ");
             String useService = sc.nextLine();
 
-            Customer customer = new Customer( nameCustomer, birthDay, sex, cmnd, phone, email, typeCustomer, address, useService);
+            Customer customer = new Customer( id,nameCustomer, birthDay, gender, cmnd, phone, email, typeCustomer, address, useService);
 
             String[] array = customer.showInfor().split(",");
 
--- src/Castudymd2/_FuramaResort/Models/Customer.java
+++ src/Castudymd2/_FuramaResort/Models/Customer.java
@@ -2,10 +2,10 @@ package Castudymd2._FuramaResort.Models;
 
 public class Customer {
     // thuộc tính:
-    
+    private String id;
     private String nameCustomer;
     private String birthDay;
-    private String sex;
+    private String gender;
     private String cmnd;
     private String phone;
     private String email;
@@ -20,10 +20,11 @@ public class Customer {
 
     }
 
-    public Customer(String nameCustomer, String birthDay, String sex, String cmnd, String phone, String email, String typeCustomer, String address, Services useService) {
+    public Customer(String id, String nameCustomer, String birthDay, String sex, String cmnd, String phone, String email, String typeCustomer, String address, Services useService) {
+        this.id = id;
         this.nameCustomer = nameCustomer;
         this.birthDay = birthDay;
-        this.sex = sex;
+        this.gender = sex;
         this.cmnd = cmnd;
         this.phone = phone;
         this.email = email;
@@ -32,10 +33,11 @@ public class Customer {
         this.useService = useService;
     }
 
-    public Customer(String nameCustomer, String birthDay, String sex, String cmnd, String phone, String email, String typeCustomer, String address, String useService) {
+    public Customer(String id, String nameCustomer, String birthDay, String sex, String cmnd, String phone, String email, String typeCustomer, String address, String useService) {
+        this.id = id;
         this.nameCustomer = nameCustomer;
         this.birthDay = birthDay;
-        this.sex = sex;
+        this.gender = sex;
         this.cmnd = cmnd;
         this.phone = phone;
         this.email = email;
@@ -46,6 +48,14 @@ public class Customer {
 
     // get,set:
 
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
 
     public String getNameCustomer() {
         return nameCustomer;
@@ -64,11 +74,11 @@ public class Customer {
     }
 
     public String getSex() {
-        return sex;
+        return gender;
     }
 
     public void setSex(String sex) {
-        this.sex = sex;
+        this.gender = sex;
     }
 
     public String getCmnd() {
@@ -120,10 +130,10 @@ public class Customer {
     }
 
     public String showInfor(){
-        return
+        return  "id='" + id + '\'' +
                 "nameCustomer='" + nameCustomer + '\'' +
                 ", birthDay='" + birthDay + '\'' +
-                ", sex='" + sex + '\'' +
+                ", gender='" + gender + '\'' +
                 ", cmnd='" + cmnd + '\'' +
                 ", phone='" + phone + '\'' +
                 ", email='" + email + '\'' +
